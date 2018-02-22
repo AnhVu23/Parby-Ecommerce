e7f4a2593d8bb41d601cebd2934fb35d3531cad4
Anh Vu <vu.haianh291@gmail.com>
2018-02-23 00:35:11 +0200

    Products Page First Commit

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
 import {MediaService} from "../../services/media";
+import {CartListPage} from "../cart-list/cart-list";
 
 /**
  * Generated class for the ProductsPage page.
@@ -15,19 +16,41 @@ import {MediaService} from "../../services/media";
   templateUrl: 'products.html',
 })
 export class ProductsPage implements OnInit{
+  cartListPage = CartListPage;
   tag = 'parby baby diaper_changing_pad';
-  mediaArray: any;
-
+  productsArray: any;
+  uploadUrl = 'http://media.mw.metropolia.fi/wbma/uploads';
+  productName: string;
+  productColor: string;
+  productSize: string;
+  productQuantity: number;
+  @ViewChild('productSlides') productSlides: Slides;
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private mediaService: MediaService) {
   }
 
   ngOnInit() {
+    this.productName = 'Diaper Changing Pad';
     this.mediaService.getImageByTag(this.tag).subscribe(
       response => {
         console.log(response);
-        this.mediaArray = response;
+        this.productsArray = response;
+      },
+      err => {
+        console.log('Products Page: ' + err);
       }
     );
+  }
+
+  onPrevSlide() {
+    this.productSlides.slidePrev();
+  }
+
+  onNextSlide() {
+    this.productSlides.slideNext();
+  }
+
+  onAddToCart() {
+    this.navCtrl.push(this.cartListPage);
   }
 }
